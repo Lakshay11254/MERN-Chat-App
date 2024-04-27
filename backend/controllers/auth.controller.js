ca4d2b39686019b790d9a2a6b1927a8662f2619a
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-04-27 02:20:38 -0400

    Connected to Mongo, onto the models section.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,18 @@
-export const signup =  (req, res) => {
+export const signup = async (req, res) => {
+    try{
+        const {fullName, username, password, confirmPassword, gender} = req.body;
+    }catch(error) {
+
+    }
     res.send("signupUser");
-    console.log("signupUser");
+    // console.log("signupUser");
 };
 
 export const login = (req, res) => {
     res.send("loginuser");
-    console.log("loginUser");
+    // console.log("loginUser");
 };
 export const logout = (req, res) => {
     res.send("logoutUser");
-    console.log("logoutUser");
+    // console.log("logoutUser");
 };  
